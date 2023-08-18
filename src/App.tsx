b4f604043a8149551e1c19acd9aee23f8666ac91
pmworkaimeiyitian <142568166+pmworkaimeiyitian@users.noreply.github.com>
2023-08-18 15:29:44 +0800

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ function App() {
   return (
     <BrowserRouter>
       <div style={{ height: '60px', width: '60px' }}>
-        <Count globalData={{}} />
+        <Count />
       </div>
     </BrowserRouter>
   );
